6852c7744ff57c0ed20334b7792e0792ff1e5fba
kouhei <aoikuma1234@gmail.com>
2021-12-21 10:00:09 +0900

    kousin

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from psycopg2 import extras
 # import pandas as pd
 
 
-# DATABASE_URL = os.environ.get("postgres://crawueupldowsl:f5db4ae256ae2d2284e35d5cfecb6e5f51ba4fd0cefe3781efc997b54e79674c@ec2-44-194-6-121.compute-1.amazonaws.com:5432/dbsb7u2apfi5h7")
+
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
@@ -32,6 +32,7 @@ while True:
     except ElementNotInteractableException:
         driver.quit()
         break
+print(soft_name_list)
 with conn.cursor() as cur:
     # テーブルを作成する SQL を準備
     sql = '''
@@ -41,8 +42,5 @@ with conn.cursor() as cur:
           );
           '''
 
-    # SQL を実行し、テーブル作成
-    extras.execute_values(cur, "INSERT INTO data (title) VALUES %s", soft_name_list)
-conn.commit()
-cur.close()
-conn.close()
+    extras.execute_values(cur, "INSERT INTO test VALUES (%s)", soft_name_list)
+    conn.commit()
